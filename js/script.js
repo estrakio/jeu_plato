7e3c91833c38421b3475e5c6b73141ee1e581ec8
estrakio <walterkarl@hotmail.fr>
2023-04-19 17:19:40 +0200

    er

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function(event){
   // permet d'ajouter un écouteur d'évènement sur le bouton et d'executer la fonction anonyme qui suis
   bouton_test.addEventListener("click", function() {
     tableau.generer_mur();
+    tableau.depart_hero();
   });
 
 });
